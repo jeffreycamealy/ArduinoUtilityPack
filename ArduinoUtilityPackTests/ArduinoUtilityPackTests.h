a74acc6329aaa6f66bce3d61406d53dd4b10be65
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-05-21 21:34:17 -0500

    Initial Commit

---
+++ ArduinoUtilityPackTests/ArduinoUtilityPackTests.h
@@ -0,0 +1,13 @@
+//
+//  ArduinoUtilityPackTests.h
+//  ArduinoUtilityPackTests
+//
+//  Created by Jeffrey Camealy on 5/21/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ArduinoUtilityPackTests : SenTestCase
+
+@end
---
+++ ArduinoUtilityPackTests/ArduinoUtilityPackTests.m
@@ -0,0 +1,32 @@
+//
+//  ArduinoUtilityPackTests.m
+//  ArduinoUtilityPackTests
+//
+//  Created by Jeffrey Camealy on 5/21/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import "ArduinoUtilityPackTests.h"
+
+@implementation ArduinoUtilityPackTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ArduinoUtilityPackTests");
+}
+
+@end
